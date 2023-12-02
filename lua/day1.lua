5c203f4c0bf577d195bf608cdf4f6025051bbe32
taehee kim <taehee.kim@wonderwall.kr>
2023-12-02 15:23:45 +0900

    feat: refactor day1 파이썬 고수의 풀이 따라해봄

--- lua/day1.lua
+++ lua/day1.lua
@@ -1,9 +1,24 @@
 local assert = require 'luassert'
 
+---@param s string
+---@param target string
+---@return boolean
+local function startswith(s, target)
+    return target ~= nil and string.sub(s, 1, #target) == target
+end
+
+---@param s string
+---@return boolean
+local function isdigit(s) return s ~= nil and s ~= "" and not s:find("%D") end
+
 local function parse_cali_value(line)
-    local first = line:match("%d")
-    local last = line:reverse():match("%d")
-    return tonumber(first .. last)
+    ---@type number[]
+    local digits = {}
+    for c in line:gmatch('.') do
+        if isdigit(c) then table.insert(digits, c) end
+    end
+
+    return tonumber(digits[1] .. digits[#digits])
 end
 
 assert.same(parse_cali_value("1abc2"), 12)
@@ -16,41 +31,23 @@ local dict = {
 }
 
 ---@param line string
-local function find_first(line)
-    local s = line:find("%d")
-    local value = line:match("%d")
-
-    for digit, word in ipairs(dict) do
-        local temp_s = line:find(word)
-        if s == nil or temp_s and temp_s < s then
-            s = temp_s
-            value = digit
-        end
-    end
-
-    return tostring(value)
-end
-
----@param line string
-local function find_last(line)
-    local reversed = line:reverse()
-    local _, e = reversed:find("%d")
-    local value = reversed:match("%d")
-
-    for digit, word in ipairs(dict) do
-        local _, temp_e = reversed:find(word:reverse())
-        if e == nil or temp_e and temp_e < e then
-            e = temp_e
-            value = digit
-        end
-    end
-
-    return tostring(value)
-end
-
 local function parse_cali_value_2(line)
-    return tonumber(find_first(line) .. find_last(line))
+    ---@type number[]
+    local digits = {}
+    for i = 1, #line do
+        local c = line:sub(i, i)
+        if isdigit(c) then table.insert(digits, c) end
+        for d, val in ipairs(dict) do
+            if startswith(line:sub(i), val) then
+                table.insert(digits, tostring(d))
+            end
+        end
+    end
+
+    return tonumber(digits[1] .. digits[#digits])
 end
+
+---@type { [1]: string, [2]: number }[]
 local cases = {
     {"two1nine", 29}, {"eightwothree", 83}, {"abcone2threexyz", 13},
     {"xtwone3four", 24}, {"4nineeightseven2", 42}, {"zoneight234", 14},
@@ -61,12 +58,13 @@ for _, case in ipairs(cases) do
     assert.same(parse_cali_value_2(case[1]), case[2], case[1])
 end
 
-local result = 0
-
+local p2 = 0
 while true do
+    ---@type string
     local line = io.read("*l")
     if line == nil then break end
-    result = result + parse_cali_value_2(line)
+
+    p2 = p2 + parse_cali_value_2(line)
 end
 
-io.write(tostring(result))
+print(tostring(p2))
